d45fd773d8e825cb80ba7b75ce972152cf8f534e
kmicic91 <bilicz.adrian@gmail.com>
2016-11-05 10:57:22 +0100

    add promoscope

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -24,5 +24,11 @@ $(window).scroll(function(){
 			},150*(i+1))
 			
 		})
+
+		if(wScroll > $('.large-window').offset().top-$(window).height()){
+			$('.large-window').css('background-position', 'center '+wScroll - $('.large-window').offset().top+'px')
+		}
+		var opacity = (wScroll - $('.large-window').offset().top)
+		$('window-tint').css('opacity', opacity)
 	}
 })
